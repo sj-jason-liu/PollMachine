9cf2a4a6d1d5985ccea893e91630eecbf434de55
sj.jason.liu <sj.jason.liu@gmail.com>
2022-09-27 17:33:47 +0800

    Added balls physics to bounce & added poll machine rotating function.

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    private Rigidbody _rb;
+
+    [SerializeField]
+    private float _speed = 1.5f;
+    
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Speedup")
+        {
+            Debug.Log("Added force");
+            Vector3 direction = transform.position - other.transform.position;
+            _rb.AddForceAtPosition(direction.normalized * _speed, transform.position);
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.tag == "Ball")
+        {
+            Vector3 direction = transform.position - other.transform.position;
+            _rb.AddForceAtPosition(direction.normalized * _speed, transform.position);
+        }
+    }
+}
---
+++ Assets/Scripts/PollWheelTuring.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PollWheelTuring : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 10f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.Rotate(0, 0, _speed * Time.deltaTime);
+    }
+}
